b5affba029f74f5fde5702e4f696bdd6af5a8433
Van Paul Dayag <vcdayag@up.edu.ph>
2023-06-08 22:43:32 +0800

    feat: add new packet

---
+++ src/main/java/io/github/p0lbang/gofish/network/packets/PacketPlayersWaiting.java
@@ -0,0 +1,9 @@
+package io.github.p0lbang.gofish.network.packets;
+
+import io.github.p0lbang.gofish.game.Player;
+
+import java.util.Map;
+
+public class PacketPlayersWaiting {
+    public Map<String, Player> PlayerMap;
+}
